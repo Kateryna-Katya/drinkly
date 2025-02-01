472b861605fadf8e92452e363f57b33b9c63985e
Kateryna-Katya <katyaukr2014@gmail.com>
2025-02-01 21:25:39 +0200

    change homePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,20 +1,15 @@
-import Header from "../../components/Header/Header";
-import WaterConsumptionTracker from "../../components/WaterConsumptionTracker/WaterConsumptionTracker";
-import WhyDrinkWater from "../../components/WhyDrinkWater/WhyDrinkWater";
 
 
-import css from "./HomePage.module.css";
+
+
 
 const HomePage = () => {
   return (
-    <div className={css.WelcomePageContainer}>
-       <Header />
-      <div className={css.welcomePage}>
-        <WaterConsumptionTracker />
-        <WhyDrinkWater />
-       
-      </div>
+    <div >
+      <h1>HomePage</h1>
+     
     </div>
+    
   );
 };
 
