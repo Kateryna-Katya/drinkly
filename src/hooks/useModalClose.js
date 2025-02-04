070c67c7c51e623c11e79f6307a9f350b3c5a958
Kseniya Semenenko <165210222+KseniyaSemenenko@users.noreply.github.com>
2025-02-04 21:58:01 +0200

    add_close_modal

---
+++ src/hooks/useModalClose.js
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+
+const useModalClose = (isOpen, onClose) => {
+  useEffect(() => {
+    const handleEscKey = (event) => {
+      if (event.key === "Escape") {
+        onClose();
+      }
+    };
+
+    if (isOpen) {
+      window.addEventListener("keydown", handleEscKey);
+    }
+
+    return () => {
+      window.removeEventListener("keydown", handleEscKey);
+    };
+  }, [isOpen, onClose]);
+
+  const handleBackdropClick = (event) => {
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  };
+
+  return { handleBackdropClick };
+};
+
+export default useModalClose;
