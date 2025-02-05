1e3fdfee6d57331bb16d5715054e454a909c5139
Alisa Kuraieva <alisa.mogila@gmail.com>
2025-02-05 13:50:16 +0200

    fixed UserLogoModal

--- src/components/UserLogoModal/UserLogoModal.jsx
+++ src/components/UserLogoModal/UserLogoModal.jsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
-import SettingModal from "../SettingModal/SettingModal";
+// import SettingModal from "../SettingModal/SettingModal";
 import UserLogoutModal from "../UserLogoutModal/UserLogoutModal";
+import { Icon } from "../Icon/Icon.jsx";
 import styles from "./UserLogoModal.module.css";
 
 const UserLogoModal = ({ onClose }) => {
-  const [isSettingModalOpen, setIsSettingModalOpen] = useState(false);
+  // const [isSettingModalOpen, setIsSettingModalOpen] = useState(false);
   const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
 
   useEffect(() => {
@@ -23,13 +24,13 @@ const UserLogoModal = ({ onClose }) => {
   return (
     <div className={styles.modalOverlay}>
       <div className={styles.userLogoModal}>
-        <button onClick={() => setIsSettingModalOpen(true)} className={styles.modalBtn}>
+        {/* <button onClick={() => setIsSettingModalOpen(true)} className={styles.modalBtn}><Icon className={styles.icon} id="icon-cog-6-tooth" width="16" height="16" />
           Setting
-        </button>
-        <button onClick={() => setIsLogoutModalOpen(true)} className={styles.modalBtn}>
+        </button> */}
+        <button onClick={() => setIsLogoutModalOpen(true)} className={styles.modalBtn}><Icon className={styles.icon} id="icon-arrow-right-on-rectanglesvg" width="16" height="16" />
           Logout
         </button>
-        {isSettingModalOpen && <SettingModal isOpen={isSettingModalOpen} onClose={() => setIsSettingModalOpen(false)} />}
+        {/* {isSettingModalOpen && <SettingModal isOpen={isSettingModalOpen} onClose={() => setIsSettingModalOpen(false)} />} */}
         {isLogoutModalOpen && <UserLogoutModal isOpen={isLogoutModalOpen} onClose={() => setIsLogoutModalOpen(false)} />}
       </div>
     </div>
