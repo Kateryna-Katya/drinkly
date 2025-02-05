3cb48b9cc2ec76e2d2daed2febcad8fd0c2f025f
NazarKopach <164062094+NazarKopach@users.noreply.github.com>
2025-02-05 15:17:54 +0100

    correct Home Page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,11 @@
 import TodayWaterList from "../../components/TodayWaterList/TodayWaterList";
 
 import css from "./HomePage.module.css";
-import DailyNormaModal from "../../components/DailyNormaModal/DailyNormaModal";
 
 const HomePage = () => {
   return (
     <div className={css.homePage}>
       <h1>HomePage</h1>
-      <DailyNormaModal />
       <div className={css.todayAndCalendarWrapper}>
         <TodayWaterList />
       </div>
