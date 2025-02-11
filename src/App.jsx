b1abaa0bc7e406895042fe5fd859bc45a8a5115e
Kateryna-Katya <katyaukr2014@gmail.com>
2025-02-11 23:56:37 +0200

    fix photo

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,16 @@ import Layout from "./components/Layout/Layout";
 import { useDispatch, useSelector } from "react-redux";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
 import RestrictedRoute from "./components/RestrictedRoute";
 import PrivateRoute from "./components/PrivateRoute";
 import { currenthUser } from "./redux/auth/operations";
-
 import { selectUserToken } from "./redux/auth/selectors";
 
 const WelcomePage = lazy(() => import("./pages/WelcomePage/WelcomePage"));
 const SignUpPage = lazy(() => import("./pages/SignUpPage/SignUpPage"));
 const SignInPage = lazy(() => import("./pages/SignInPage/SignInPage"));
 const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
-const ForgotPasswordPage = lazy(() =>
-  import("./pages/ForgotPasswordPage/ForgotPasswordPage")
-);
+const ForgotPasswordPage = lazy(() => import("./pages/ForgotPasswordPage/ForgotPasswordPage"));
 
 const App = () => {
   const dispatch = useDispatch();
